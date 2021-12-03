ab9666051a271da61adbd82354f779502e66e6fe
EdwardObala1 <e.obala@alustudent.com>
2021-12-03 10:12:58 +0200

    Users configured into boooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  
 
@@ -11,6 +9,16 @@ Rails.application.routes.draw do
   delete '/Libraries/:id', to: 'libraries#destroy', as: 'library_delete'
   get '/Libraries/new', to: 'libraries#new', as: 'new_library'
   patch '/libraries/:id', to: 'libraries#update', as: 'update'
+  post '/libraries/:id/borrow', to: 'libraries#borrow', as: 'borrow_book'
 
   # users
+
+  get '/users', to: 'users#index', as: 'users'
+  post '/users', to: 'users#create'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  get '/users/:id', to: 'users#edit', as: 'user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
 end
